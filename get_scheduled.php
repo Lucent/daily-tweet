06f4ef36f915e8467559d85e35b38a647f9769f7
Michael Dayah <michael@dayah.com>
2020-05-29 02:11:46 -0400

    Testing two API methods to get scheduled tweets. Only one works.

---
+++ get_scheduled.php
@@ -0,0 +1,37 @@
+<?php
+
+$COUNT = 30;
+$ACCOUNT = "q23p";
+$by_id = [];
+
+$next_cursor = "";
+do {
+	echo "LOOP RUN\n";
+	$scheduled = fetch_scheduled($next_cursor);
+	foreach ($scheduled["data"] as $tweet)
+		if ($tweet["scheduled_status"] !== "SUCCESS")
+			add_tweet($by_id, $tweet);
+	$next_cursor = $scheduled["next_cursor"];
+} while ($next_cursor);
+
+array_multisort(array_column($by_id, "date"), SORT_ASC, $by_id);
+
+//print_r($by_id);
+
+function fetch_scheduled($cursor = "") {
+	echo $cursor, "\n";
+	global $COUNT, $ACCOUNT;
+	if ($cursor !== "")
+		$cursor = "&cursor={$cursor}";
+	$scheduled = `twurl -H "ads-api.twitter.com" "/7/accounts/{$ACCOUNT}/scheduled_tweets?count={$COUNT}{$cursor}"`;
+	return json_decode($scheduled, true);
+}
+
+function add_tweet(&$by_id, $tweet) {
+	$by_id[$tweet["id"]] = [
+		"date" => $tweet["scheduled_at"],
+		"text" => $tweet["text"]
+	];
+}
+
+?>
